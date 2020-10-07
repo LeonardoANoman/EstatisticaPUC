3ec372bc61933bcad2d583f7ccfc6d3256399430
Leonardo Antunes <leonoman07@gmail.com>
2020-10-07 03:59:47 -0300

    Adicionado classe main para utilização dos algoritmos.

---
+++ App.java
@@ -0,0 +1,11 @@
+import FuncoesUtil.Fatorial;
+
+public class App {
+
+    /*
+     * Classe Main para testar funções
+     */
+    public static void main(String[] args) {
+        System.out.println(Fatorial.fatorial(5));
+    }
+}
